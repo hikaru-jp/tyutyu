001f78856246bb2cefbc8a070111357f8536e4a0
yonehara hikaru <yoneharahikaru1212@gmail.com>
2026-07-19 12:23:51 +0900

    Initial commit

---
+++ bookshelf-app/lang/ja/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'required' => ':attribute を入力してください。',
+    'email' => ':attribute は有効なメールアドレス形式で入力してください。',
+    'max' => [
+        'string' => ':attribute は:max文字以内で入力してください。',
+    ],
+    'min' => [
+        'string' => ':attribute は:min文字以上で入力してください。',
+    ],
+    'confirmed' => ':attribute が一致しません。',
+
+    'attributes' => [
+        'name' => '名前',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+        'title' => 'タイトル',
+        'author' => '著者',
+    ],
+
+];
